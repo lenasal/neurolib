db4b191656a1a496578f336491de923b16b38744
Lena Salfenmoser <lena.salfenmoser@tu-berlin.de>
2022-11-30 13:08:26 +0100

    implemented 3 sparsity functionals plus tests

--- neurolib/optimal_control/cost_functions.py
+++ neurolib/optimal_control/cost_functions.py
@@ -81,13 +81,13 @@ def energy_cost(u, w_2, dt):
     :param u:   Control-dimensions x T array. Control signals.
     :type u:    np.ndarray
 
-    :param w_2: Weight that is multiplied with the W2 ("energy") cost.
+    :param w_2: Weight that is multiplied with the L2 ("energy") cost.
     :type w_2:  float
 
     :param dt:  Time step.
     :type dt:   float
 
-    :return:    W2 cost of the control.
+    :return:    L2 cost of the control.
     :rtype:     float
     """
     return w_2 * 0.5 * np.sum(u**2.0) * dt
@@ -99,10 +99,130 @@ def derivative_energy_cost(u, w_2):
     :param u:   Control-dimensions x T array. Control signals.
     :type u:    np.ndarray
 
-    :param w_2: Weight that is multiplied with the W2 ("energy") cost.
+    :param w_2: Weight that is multiplied with the L2 ("energy") cost.
     :type w_2:  float
 
-    :return :   Control-dimensions x T array of W2-cost gradients.
+    :return :   Control-dimensions x T array of L2-cost gradients.
     :rtype:     np.ndarray
     """
     return w_2 * u
+
+
+@numba.njit
+def L1_cost(u, w_1, dt):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1: Weight that is multiplied with the L1 ("sparsity") cost.
+    :type w_1:  float
+
+    :param dt:  Time step.
+    :type dt:   float
+
+    :return:    L1 cost of the control.
+    :rtype:     float
+    """
+    return w_1 * np.sum(np.abs(u)) * dt
+
+
+@numba.njit
+def derivative_L1_cost(u, w_1):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1: Weight that is multiplied with the L1 ("sparsity") cost.
+    :type w_1:  float
+
+    :return :   Control-dimensions x T array of L1-cost gradients.
+    :rtype:     np.ndarray
+    """
+    return w_1 * np.sign(u)
+
+
+@numba.njit
+def L1T_cost(u, w_1T, dt):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1T: Weight that is multiplied with the L1T ("temporal sparsity") cost.
+    :type w_1T:  float
+
+    :param dt:  Time step.
+    :type dt:   float
+
+    :return:    L1T cost of the control.
+    :rtype:     float
+    """
+
+    return w_1T * np.sqrt(np.sum(np.sum(np.sum(np.abs(u), axis=1), axis=0) ** 2) * dt)
+
+
+@numba.njit
+def derivative_L1T_cost(u, w_1T, dt):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1T: Weight that is multiplied with the L1T ("temporal sparsity") cost.
+    :type w_1T:  float
+
+    :param dt:  Time step.
+    :type dt:   float
+
+    :return :   Control-dimensions x T array of L1T-cost gradients.
+    :rtype:     np.ndarray
+    """
+
+    denominator = L1T_cost(u, w_1T, dt)
+    if denominator == 0.0:
+        return np.zeros((u.shape))
+
+    return w_1T * np.sum(np.sum(np.abs(u), axis=1), axis=0) * np.sign(u) / denominator
+
+
+@numba.njit
+def L1D_cost(u, w_1D, dt):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1D: Weight that is multiplied with the L1D ("directional sparsity") cost.
+    :type w_1D:  float
+
+    :param dt:  Time step.
+    :type dt:   float
+
+    :return:    L1D cost of the control.
+    :rtype:     float
+    """
+
+    return w_1D * np.sum(np.sum(np.sqrt(np.sum(u**2, axis=2) * dt), axis=1), axis=0)
+
+
+@numba.njit
+def derivative_L1D_cost(u, w_1D, dt):
+    """
+    :param u:   Control-dimensions x T array. Control signals.
+    :type u:    np.ndarray
+
+    :param w_1D: Weight that is multiplied with the L1D ("directional sparsity") cost.
+    :type w_1D:  float
+
+    :param dt:  Time step.
+    :type dt:   float
+
+    :return :   Control-dimensions x T array of L1D-cost gradients.
+    :rtype:     np.ndarray
+    """
+
+    denominator = np.sqrt(np.sum(u**2, axis=2) * dt)
+    der = np.zeros((u.shape))
+    for n in range(der.shape[0]):
+        for v in range(der.shape[1]):
+            if denominator[n, v] != 0.0:
+                der[n, v, :] = w_1D * u[n, v, :] / denominator[n, v]
+
+    return der
