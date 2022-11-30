dfdbbc0a413b303c93a68a08da0ecaaf4bcea5ac
Lena Salfenmoser <lena.salfenmoser@tu-berlin.de>
2022-11-30 14:44:25 +0100

    new branch for chaos experiments

--- neurolib/optimal_control/oc_fhn/oc_fhn.py
+++ neurolib/optimal_control/oc_fhn/oc_fhn.py
@@ -147,6 +147,9 @@ class OcFhn(OC):
         target,
         w_p=1.0,
         w_2=1.0,
+        w_1=0.0,
+        w_1T=0.0,
+        w_1D=0.0,
         maximum_control_strength=None,
         print_array=[],
         precision_cost_interval=(None, None),
@@ -161,6 +164,9 @@ class OcFhn(OC):
             target,
             w_p=w_p,
             w_2=w_2,
+            w_1=w_1,
+            w_1T=w_1T,
+            w_1D=w_1D,
             maximum_control_strength=maximum_control_strength,
             print_array=print_array,
             precision_cost_interval=precision_cost_interval,
@@ -264,8 +270,15 @@ class OcFhn(OC):
         Du @ fk + adjoint_k.T @ Du @ h
         """
         self.solve_adjoint()
-        fk = cost_functions.derivative_energy_cost(self.control, self.w_2)
-
+        du_f = np.zeros((self.control.shape))
+        if self.w_2 != 0.0:
+            du_f += cost_functions.derivative_energy_cost(self.control, self.w_2)
+        if self.w_1 != 0.0:
+            du_f += cost_functions.derivative_L1_cost(self.control, self.w_1)
+        if self.w_1T != 0.0:
+            du_f += cost_functions.derivative_L1T_cost(self.control, self.w_1T, self.dt)
+        if self.w_1D != 0.0:
+            du_f += cost_functions.derivative_L1D_cost(self.control, self.w_1D, self.dt)
         duh = self.Duh()
 
-        return compute_gradient(self.N, self.dim_out, self.T, fk, self.adjoint_state, self.control_matrix, duh)
+        return compute_gradient(self.N, self.dim_out, self.T, du_f, self.adjoint_state, self.control_matrix, duh)
